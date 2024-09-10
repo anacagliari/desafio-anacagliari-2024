6cfaf2a1a9db821da8e9b5526bab584ab6840cad
Ana Cagliari <carolinecagliari@gmail.com>
2024-09-10 15:25:06 -0300

    primeira versão de dados

---
+++ src/data/animaisData.js
@@ -0,0 +1,8 @@
+export const animais = [
+    { especie: 'LEAO', tamanho: 3, biomas: ['savana'], carnivoro: true },
+    { especie: 'LEOPARDO', tamanho: 2, biomas: ['savana'], carnivoro: true },
+    { especie: 'CROCODILO', tamanho: 3, biomas: ['rio'], carnivoro: true },
+    { especie: 'MACACO', tamanho: 1, biomas: ['savana', 'floresta'], carnivoro: false },
+    { especie: 'GAZELA', tamanho: 2, biomas: ['savana'], carnivoro: false },
+    { especie: 'HIPOPOTAMO', tamanho: 4, biomas: ['savana', 'rio'], carnivoro: false }
+];
---
+++ src/data/recintosData.js
@@ -0,0 +1,7 @@
+export const recintos = [
+    { numero: 1, bioma: ['savana'], tamanhoTotal: 10, animaisExistentes: [{ especie: 'MACACO', quantidade: 3 }] },
+    { numero: 2, bioma: ['floresta'], tamanhoTotal: 5, animaisExistentes: [] },
+    { numero: 3, bioma: ['savana', 'rio'], tamanhoTotal: 7, animaisExistentes: [{ especie: 'GAZELA', quantidade: 1 }] },
+    { numero: 4, bioma: ['rio'], tamanhoTotal: 8, animaisExistentes: [] },
+    { numero: 5, bioma: ['savana'], tamanhoTotal: 9, animaisExistentes: [{ especie: 'LEAO', quantidade: 1 }] }
+];
