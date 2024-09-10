96c6f4671de6ab73ead4abddb0f1c9be7f4bdef2
Ana Cagliari <carolinecagliari@gmail.com>
2024-09-10 16:07:33 -0300

    verificação de quantidade inválida

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -4,10 +4,17 @@ class RecintosZoo {
 
     analisaRecintos(animal, quantidade) {
         const animaisValidos = animais.map(animal => animal.especie);
-        
         if (!animaisValidos.includes(animal)) {
             return {
                 erro: "Animal inválido",
+                recintosViaveis: false
+            };
+        }
+
+        const quantidadeValida = (typeof quantidade === 'number' && quantidade > 0 && quantidade !== null);
+        if (!quantidadeValida) {
+            return {
+                erro: "Quantidade inválida",
                 recintosViaveis: false
             };
         }
