63817d11668674c2b511fb03c64d8cb4483b4fd2
Ana Cagliari <carolinecagliari@gmail.com>
2024-09-10 15:59:04 -0300

    verificação de animal inválido

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,8 +1,17 @@
+import  { animais } from './data/animaisData.js';
+
 class RecintosZoo {
 
     analisaRecintos(animal, quantidade) {
+        const animaisValidos = animais.map(animal => animal.especie);
+        
+        if (!animaisValidos.includes(animal)) {
+            return {
+                erro: "Animal inválido",
+                recintosViaveis: false
+            };
+        }
     }
-
 }
 
 export { RecintosZoo as RecintosZoo };
